1ed6fd74592814aeb8b7051a243c075414686113
Кирилл Щербаков <kshcherbakov@LIVE.RU>
2022-04-03 01:06:34 +0300

    prices added

--- PriceSearcher/Form1.cs
+++ PriceSearcher/Form1.cs
@@ -20,8 +20,10 @@ namespace PriceSearcher
         private void button1_Click(object sender, EventArgs e)
         {
             Parcer zapros = new Parcer();
-            string a = zapros.items_selection(textBox1.Text);
-            label1.Text = a;
+            zapros.items_selection(textBox1.Text);
+            label1.Text = zapros.names[0] + "    " + zapros.prices[0];
+            label2.Text = zapros.names[1] + "    " + zapros.prices[1];
+            label3.Text = zapros.names[2] + "    " + zapros.prices[2];
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -37,11 +39,12 @@ namespace PriceSearcher
 
     public class Parcer
     {
-        string name = string.Empty;
-        string link = string.Empty;
-        string price = string.Empty;
-        public string items_selection(string item)
+        public List<string> prices = new List<string>();
+        public List<string> names = new List<string>();
+        public List<string> links = new List<string>();
+        public void items_selection(string item)
         {
+            
             //HtmlWeb web = new HtmlWeb();
             // HtmlAgilityPack.HtmlDocument doc = web.Load("https://shop-lot.ru/search/?s=%D0%A1%D0%A2%D0%98%D0%A0%D0%90%D0%9B%D0%AC%D0%9D%D0%90%D0%AF+%D0%9C%D0%90%D0%A8%D0%98%D0%9D%D0%90");
             HtmlWeb web = new HtmlWeb();
@@ -50,21 +53,26 @@ namespace PriceSearcher
 
             var liNodes = doc.DocumentNode.SelectNodes("//div[@class='prod_info']");
             var list_q = liNodes.ToList();
-            List<string> names_ = new List<string>();
             foreach (var ll in list_q)
             {
-
                 string qq = ll.InnerHtml;
                 var d = new HtmlAgilityPack.HtmlDocument();
                 d.LoadHtml(qq);
-
                 HtmlNode name_nod = d.DocumentNode.SelectSingleNode("//span[@itemprop='name']");
                 string name = name_nod.InnerText;
-                names_.Add(name);
+                names.Add(name);
             }
-            
-            
-            return name;
+            foreach (var ll in list_q)
+            {
+                string qq = ll.InnerHtml;
+                var d = new HtmlAgilityPack.HtmlDocument();
+                d.LoadHtml(qq);
+                HtmlNode price_nod = d.DocumentNode.SelectSingleNode("//span[@class='price_num']");
+                string price = price_nod.InnerText;
+                prices.Add(price);
+            }
+           
+
         }
     }
 }
